1d555cfdddcf672a1ea6f250180aa6171751d646
Ali <ali.boostanafrooz@yahoo.com>
2020-01-22 23:58:54 +0330

    My first web and application code

--- application/models/Display_model.php
+++ application/models/Display_model.php
@@ -3,31 +3,33 @@
 
 class Display_model extends CI_Model
 {
-		public function load_all_applications(){
-	return	$this->db->get('application');
+	public function load_all_applications()
+	{
+		return $this->db->get('applications');
 
-		}
+	}
 
+	//First Page Categories
 
-		//First Page Categories
+	public function get_best_new_apps()
+	{
+		$this->db->limit(2);
+		$r = $this->db->get('applications');
+		return $r;
+	}
 
-		public function get_best_new_apps()
-		{
-			$this->db->limit(3);
-			$q= $this->db->get('application');
-			return $q;
-		}
-		public function get_best_new_games()
-		{
-			$this->db->limit(3);
-			$this->db->where('isgame',1);
-			$q= $this->db->get('application');
-			return $q;
-		}
-		public function get_best_new_updates()
-		{
-			$this->db->limit(3);
-			$q= $this->db->get('application');
-			return $q;
-		}
+	public function get_best_new_games()
+	{
+		$this->db->limit(2);
+		$this->db->where('isgame', 1);
+		$r = $this->db->get('applications');
+		return $r;
+	}
+
+	public function get_best_new_updates()
+	{
+		$this->db->limit(1);
+		$r = $this->db->get('applications');
+		return $r;
+	}
 }
--- application/views/display/all.php
+++ application/views/display/all.php
@@ -2,19 +2,19 @@
 	<div class="s-app-group-title"> برنامه های جدید برگزیده </div>
 	<?php
 	foreach ($applications as $application) { ?>
-	<div class="s-app-panel">
-		<a href="<?php echo base_url('index.php/display/app'); ?>">
-			<img src=" <?php echo base_url('Assets/img/apps/icon/icon.svg') ?>" class="s-app-icon"/>
-			<div class="s-app-name"> <?php echo $application->name; ?> </div>
-			<?php if ($application->price == 0) {
-				?>
-				<div class="s-app-price">رایگان</div>
-				<?php if ($application->inpay == 1) { ?>
-					<div class="s-app-price">پرداخت درون برنامه ای</div>
+		<div class="s-app-panel">
+			<a href="<?php echo base_url('index.php/display/app'); ?>">
+				<img src=" <?php echo base_url('Assets/img/apps/icon/icon.svg') ?>" class="s-app-icon"/>
+				<div class="s-app-name"> <?php echo $application->name; ?> </div>
+
+				<?php if ($application->price == 0) { ?>
+					<div class="s-app-price">رایگان</div>
+					<?php if ($application->inpay == 1) { ?>
+						<div class="s-app-price">پرداخت درون برنامه ای</div>
+					<?php } ?>
+				<?php } else { ?>
+					<div class="s-app-price">  <?php echo 'تومان' . $application->price; ?> </div>
 				<?php } ?>
-			<?php } else { ?>
-				<div class="s-app-price">  <?php echo 'تومان' . $application->price; ?> </div>
-			<?php } ?>
-		</a>
-	</div>
-<?php } ?>
+			</a>
+		</div>
+	<?php } ?>
